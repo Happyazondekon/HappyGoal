b0ac9e57ebec6100bf8bbd4bf88e18d58ef3447f
happyazondekon <happyazondekon@gmail.com>
2025-06-06 19:40:52 +0100

    maj team color

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const Color secondary = Color(0xFF2196F3);
   static const Color background = Color(0xFFF5F5F5);
   static const Color fieldGreen = Color(0xFF4CAF50);
-  static const Color team1 = Color(0xFFE53935);
+  static const Color team1 = Color(0xFF1E88E5);
   static const Color team2 = Color(0xFF1E88E5);
 }
 
